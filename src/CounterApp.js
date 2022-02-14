914ef5a95a255efc08740e2d5e579aed6cab3afa
FrancisTocino <francistocino@hotmail.com>
2022-02-14 10:41:17 +0100

    Hasta el Tema 41

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,28 +2,29 @@ import React from "react";
 import PropTypes from "prop-types"; // Hay que importar esto para
 // que se pueda hacer validación de las props
 
-const PrimeraApp = ({ saludo }) => {
+const CounterApp = ({value}) => {
 
-    console.log (saludo);
+    console.log (value);
     return (
         <>
-            <h1>HOLA {saludo} </h1>
-            <h4>Mi Primera Aplicación</h4>
+            <h1>CounterApp</h1>
+            <h4> { value } </h4>
         </>
     );
 }
 
-PrimeraApp.propTypes = {
-    saludo: PropTypes.string.isRequired,
+
+CounterApp.propTypes = {
+   value: PropTypes.number.isRequired,
 } // Esto es para que sea obligatorio enviar los prop
   // y tambien que sea de tipo string.
 
 
-  PrimeraApp.defaultProps  = {
-        saludo: 'Saludo por Defecto',
+  CounterApp.defaultProps  = {
+    value: 909090,
   } // Esto es otra forma de poner valores
     // por defectos a las props sin tener 
     // que guarrear arriba
 
-export default PrimeraApp;
+export default CounterApp;
 
